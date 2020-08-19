93a8f8ddbcc4f7079134e4667797c1d1fe5f380e
Squareben1 <bengittins@live.com>
2020-08-19 21:45:52 +0100

    Refactored #graph_arr_generator

--- timer.py
+++ timer.py
@@ -23,31 +23,17 @@ class AlgoTimer:
         for i in range(10):
             total_time += self.basic_timer(func, *args)
         avg_time = total_time / 10
-        # print('Average time to run', func.__name__,
-        #       'with an array', len(args[0]), 'items long:')
-        # average = '%.12f' % avg_time
         return avg_time
 
     def graph_arr_generator(self, interval, limit):
         output_arr = []
-
-        if limit == 1:
-            return [[1]]
-        elif limit == 2:
-            return [[1], [1, 2]]
-        else:
-            for i in range(1, 6):
-                output_arr.append(list(range(1, i + 1)))
-            return output_arr
-# steps
-
-        # = limit // interval
-        # for i in range(1, steps + 1):
-        #     arr = list(range(i * interval))
-        #     return arr
+        for i in range(1, limit+1):
+            output_arr.append(list(range(1, i + 1)))
+        return output_arr
 
 
-# algo_timer = AlgoTimer()
-
-# algo_timer.get_average(binary_search, arr, rand_num)
-# get_average(random.shuffle(arr))
+"""
+        print('Average time to run', func.__name__,
+              'with an array', len(args[0]), 'items long:')
+        average = '%.12f' % avg_time
+"""
