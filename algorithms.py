eba3ce69e513432fd090a53660e9ac2278828ce4
Squareben1 <bengittins@live.com>
2020-09-16 21:11:43 +0100

    Refactored and added TODO to speed up

--- algorithms.py
+++ algorithms.py
@@ -24,8 +24,28 @@ def find_dups(arr):
 
     for i in range(len(arr)):
         for y in range(i+1, len(arr)):
-            if arr[i] == arr[y]:
-                if arr[i] not in return_arr:
-                    return_arr.append(arr[i])
-                    break
+            if arr[i] == arr[y] and arr[i] not in return_arr:
+                return_arr.append(arr[i])
+                break
     return return_arr
+
+
+# TODO incorporate sorted list to speed up and remove nested for loop
+"""
+sort list A
+sort list B (according to the same sort order)
+I = 0
+J = 0
+repeat
+  X = A[I]
+  Y = B[J]
+  if X matches Y then
+    add (X, Y) to results
+    increment I
+    increment J
+  else if X < Y then
+    increment I
+  else increment J
+until either index reaches the end of its list
+If you do it this way, instead of the number of matches operations being based on length(A) * length(B), it's now based on length(A) + length(B), which means your code will run much faster.
+"""
