9e711a8a391f9c67b15063c4c43fcc9d048968b4
Squareben1 <bengittins@live.com>
2020-09-20 14:09:58 +0100

    Created #insert_dup to return arrays with duplicates

--- arr_generators.py
+++ arr_generators.py
@@ -7,3 +7,10 @@ def arr_generator(num_arrs, limit):
     for i in range(1, num_arrs+1):
         output_arr.append(list(range(1, (i * step)+1)))
     return output_arr
+
+
+def insert_dup(arr):
+    for sub_arr in arr:
+        for i in range(0, len(sub_arr), 10):
+            sub_arr[i] += 1
+    return arr
