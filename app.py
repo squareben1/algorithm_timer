416f5500429079cf90572add8d5395ce4b27e921
mattyocode <matthewoliver@live.co.uk>
2020-09-27 16:55:04 +0100

    Further refactor + better naming

--- app.py
+++ app.py
@@ -13,28 +13,23 @@ class AlgoApp:
         self.arr_length = []
         self.run_times = 5
 
-    def select_run_type(self, algorithm):
-        method_to_call = getattr(algorithms, algorithm)
-    
-        self.get_average_with_random_num(algorithm)
-
-        create_graph(self.time_results, self.arr_length, method_to_call.__name__)
-
-    def get_average_with_random_num(self, algorithm):  # rename
+    def run(self, algorithm):
         algorithm_to_call = getattr(algorithms, algorithm)
         self.set_test_arr(algorithm)
 
         if 'search' in algorithm:
-            function = self.search_for_random_num
-        else: 
-            function = self.run_sort_algo
+            selected_function = self.search_for_random_num
+        else:
+            selected_function = self.run_sort_algo
 
         for arr in self.test_arr:
             self.arr_length.append(len(arr))
-            result = function(algorithm_to_call, arr)
-            self.time_results.append(self.get_average(result))
-            
-        return self.time_results, self.arr_length
+            result = selected_function(algorithm_to_call, arr)
+            self.time_results.append(
+                self.get_average_selected_time_data(result))
+
+        create_graph(self.time_results, self.arr_length,
+                     algorithm_to_call.__name__)
 
     def set_test_arr(self, algorithm):
         if 'dups' in algorithm:
@@ -56,7 +51,7 @@ class AlgoApp:
             hold_arr.append(output[0])
         return hold_arr
 
-    def get_average(self, result):
+    def get_average_selected_time_data(self, result):
         sliced_results = self.slice_outlier_results(result)
         average = sum(sliced_results) / len(sliced_results)
         return average
@@ -68,11 +63,12 @@ class AlgoApp:
             end_slice = len(results_array) - pc_to_cut
         return results_array[pc_to_cut:end_slice]
 
+
 if __name__ == "__main__":
     import sys
     import algorithms
     app = AlgoApp(sys.argv[1], sys.argv[2])
-    app.select_run_type(sys.argv[3])
+    app.run(sys.argv[3])
 
 # TODO write algorithms for shuffling, sorting and reversing
-# TODO remove number of arrays as argument 
+# TODO remove number of arrays as argument
