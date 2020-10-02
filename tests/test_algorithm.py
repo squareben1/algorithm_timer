3a1f90219f334c578944f26cef3eaa20321d45ec
mattyocode <matthewoliver@live.co.uk>
2020-10-02 14:04:50 +0100

    #linear search tested

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -20,16 +20,22 @@ class TestFindDups:
         assert find_dups([1, 2, 2, 2]) == [2]
 
 
-test_list = [1, 3, 5, 7, 9]
+example_arr = [1, 3, 5, 7, 9]
 
 class TestBinarySearch:
     
 
     def test_binary_search_3(self):
-        assert binary_search(test_list, 3) == 1
+        assert binary_search(example_arr, 3) == 1
 
 
     def test_binary_search_9(self):
-        assert binary_search(test_list, 9) == 4
+        assert binary_search(example_arr, 9) == 4
 
-    
+
+class TestLinearSearch:
+    def test_linear_search_not_found(self):
+        assert linear_search(example_arr, 10) == False
+
+    def test_linear_search_found(self):
+        assert linear_search(example_arr, 7) == True
