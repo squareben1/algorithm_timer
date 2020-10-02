2797959bb6021dbbeaf8d5155ac5dd586b9e39dd
Squareben1 <bengittins@live.com>
2020-10-02 11:50:46 +0100

    Unit test for #get_average_selected_time_data

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,8 +4,16 @@ from app import AlgoApp
 
 subject = AlgoApp(10, 5)
 
+arr_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 def test_slice_outlier_results():
     arr_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     expected = [2, 3, 4, 5, 6, 7, 8, 9]
     assert subject.slice_outlier_results(arr_input) == expected
+
+
+def test_get_average_selected_time_data():
+    sliced_arr = [2, 3, 4, 5, 6, 7, 8, 9]
+    expected = sum(sliced_arr) / len(sliced_arr)
+    assert subject.get_average_selected_time_data(arr_input) == expected
